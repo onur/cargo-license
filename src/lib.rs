12024c34dca38c1992fcb258c4ba11f152c5d5ae
Ryo Yamashita <qryxip@gmail.com>
2021-01-04 23:46:19 +0900

    Fix a Clippy lint

--- src/lib.rs
+++ src/lib.rs
@@ -59,7 +59,7 @@ pub fn get_dependencies_from_cargo_lock(
     for package in metadata.packages {
         detailed_dependencies.push(DependencyDetails::new(&package));
     }
-    detailed_dependencies.sort_by(|dep1, dep2| dep1.cmp(dep2));
+    detailed_dependencies.sort_unstable();
     Ok(detailed_dependencies)
 }
 
